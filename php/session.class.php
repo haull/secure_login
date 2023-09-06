6a30eb51fcee0926300e32f5c2992c4bf8222752
haul <66765822+haull@users.noreply.github.com>
2023-09-06 13:24:01 +0700

    Add files via upload

---
+++ php/session.class.php
@@ -0,0 +1,68 @@
+<?php
+class Session {
+    private static function startSession() {
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_name('sws_session');
+            session_start();
+        }
+    }
+    static function setUser($user) {
+        $success = false;
+        if (!is_a($user, 'User')) {
+            // log possible cookie tampering
+        } else if (self::startSession() && session_regenerate_id(true) && session_id()) {
+            $_SESSION['user'] = $user;
+            $success = true;
+        }
+        return $success;
+    }
+    static function getUser() {
+        $user = null;
+        if (self::startSession() && isset($_SESSION['user'])) {
+            if (!is_a($_SESSION['user'], 'User')) {
+                // log possible cookie tampering
+                self::deleteSession();
+            } else if ($_SESSION['user']->getRemoteAddr() !== $_SERVER['REMOTE_ADDR']) {
+                // log possible session hijacking
+                // need to test using VPN
+                self::deleteSession();
+            } else {
+                $user = $_SESSION['user'];
+            }
+        }
+        return $user;
+    }
+    static function deleteSession() {
+        if (self::startSession()) {
+            if (ini_get('session.use_cookies') !== false) {
+                $parameters = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000, $parameters['path'], $parameters['domain'], $parameters['secure'], $parameters['httponly']);
+            }
+            session_destroy();
+        }
+    }
+    // synchronizer token pattern | unique token per-request
+    public function generateToken($name) {
+        $token = null;
+        $name = "{$name}_csrf";
+        if (self::startSession()) {
+            $_SESSION['tokens'][$name] = array('value' => bin2hex(openssl_random_pseudo_bytes(32)), 'expiration' => time() + 60 * 15);
+            $token = $_SESSION['tokens'][$name]['value'];
+        }
+        return $token;
+    }
+    public function verifyToken($name, $value) {
+        $success = false;
+        $name = "{$name}_csrf";
+        if (self::startSession() && isset($_SESSION['tokens'][$name])) {
+            if ($value !== $_SESSION['tokens'][$name]['value'] || time() > $_SESSION['tokens'][$name]['expiration']) {
+                // log possible CSRF (Cross-Site Request Forgery)
+            } else {
+                unset($_SESSION['tokens'][$name]);
+                $success = true;
+            }
+        }
+        return $success;
+    }
+}
+?>
